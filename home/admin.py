eeb191f8fdaf26ad1feaedad9ddbb84a5d60b27c
SUBHAM KUMAR PANDEY <63279335+SubhamKumarPandey@users.noreply.github.com>
2020-10-28 13:13:02 +0530

    Add files via upload

---
+++ home/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from home.models import Contact,Reliefcamp,Aboutngo, Data1 ,Data2, Data3,Data4,Rivers,Donation,Location,Issue
+
+
+# Register your models here.
+admin.site.register(Contact)
+
+admin.site.register(Donation)
+
+admin.site.register(Issue)
+
+admin.site.register(Location)
+
+admin.site.register(Reliefcamp)
+
+admin.site.register(Aboutngo)
+
+admin.site.register(Data1)
+
+admin.site.register(Data2)
+
+admin.site.register(Data3)
+
+admin.site.register(Data4)
+
+admin.site.register(Rivers)
+
